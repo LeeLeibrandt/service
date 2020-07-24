2ec2ced3c11f736586631f470383cc99b98db580
Lee Leibrandt <leeleibrandt9@gmail.com>
2020-07-24 13:09:11 +0200

    real-time clock

--- js/main.js
+++ js/main.js
@@ -37,4 +37,19 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
-
+//REAL TIME CLOCK
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
